c09583587ae2d2ca962bdd9bdf9b659622ed168e
Jessica Ferrari <jscferrari@gmail.com>
2024-12-31 09:16:51 -0300

    config(jmoneyapi): Configure project container

---
+++ app.py
@@ -0,0 +1,45 @@
+import os
+
+from flask import Flask
+from flask_cors import CORS
+from flask_restx import Api
+from flask_migrate import Migrate
+
+from database import db
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    cors = CORS(app)
+    app.config.from_pyfile('/app/settings/settings.py', silent=False)
+
+    # configure the app    
+    app.config.from_mapping(
+        secret_key='dev',
+    )
+    
+    if test_config is not None:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    db.init_app(app)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    return app
+
+def init_api(app):
+    return Api(app, version='1.0', title='jMoney API',
+        description='A Rest API to manage personal finances',
+    )
+
+
+app = create_app()
+api = init_api(app)
+
+migrate = Migrate(app, db)
+
